fde298c401169ab39c85fc977f249f832db4bf5e
Boran Koeruek <external.boran.koeruek@thyssenkrupp.com>
2023-05-26 16:58:57 +0200

    Added bootstrap dependency, generated header and navbar components without content, added application frame module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlgoVisualizerComponent } from './modules/component/algo-visualizer/algo-visualizer.component';
+import { HeaderComponent } from './application-frame/component/header/header.component';
+import { ApplicationFrameModule } from './application-frame/application-frame.module';
 
 @NgModule({
   declarations: [AppComponent, AlgoVisualizerComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ApplicationFrameModule],
   providers: [],
   bootstrap: [AppComponent],
 })
