e90c2fd02f8220a6fa7bb815d05b3ebccdd208bb
Boran Koeruek <external.boran.koeruek@thyssenkrupp.com>
2023-05-26 14:49:09 +0200

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { AlgoVisualizerComponent } from './modules/component/algo-visualizer/algo-visualizer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: AlgoVisualizerComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/modules/component/algo-visualizer/algo-visualizer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-algo-visualizer',
+  templateUrl: './algo-visualizer.component.html',
+  styleUrls: ['./algo-visualizer.component.scss']
+})
+export class AlgoVisualizerComponent {
+
+}
