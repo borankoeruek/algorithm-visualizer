037ef49ea726cb37626ffe805b6695fb8f128c13
Boran Koeruek <borankrk@gmail.com>
2023-05-27 23:04:36 +0200

    Added header menu for algorithm selector

--- src/app/application-frame/application-frame.module.ts
+++ src/app/application-frame/application-frame.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 import { HeaderComponent } from './component/header/header.component';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatMenuModule],
   exports: [HeaderComponent],
 })
 export class ApplicationFrameModule {}
